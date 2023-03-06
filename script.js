9398fa94569b09e0e9d6cd1ad4cdedaf75f01591
0sakib0 <88287970+0sakib0@users.noreply.github.com>
2023-03-07 00:19:41 +0600

    add display functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test");
+console.log("mango");
+
+const buttons = document.querySelectorAll('button');
+const result = document.getElementById('result');
+let display = "";
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonValue = button.value;
+        display += buttonValue;
+        result.value = display;
+    });
+});
+
+
