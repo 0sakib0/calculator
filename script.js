08b0d91b95783221ca7246319b2d660feb8fe5b5
0sakib0 <88287970+0sakib0@users.noreply.github.com>
2023-03-07 19:54:40 +0600

    add button click sound

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 console.log("mango");
-
-const buttons = document.querySelectorAll('button');
-const result = document.getElementById('result');
+const buttonSound = document.getElementById('button-sound');
+const buttons = document.querySelectorAll("button");
+const result = document.getElementById("result");
+const acButton = document.querySelector(".all-clear");
 let display = "";
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
         const buttonValue = button.value;
-        display
-        if (button.classList.contains('number')) {
+        buttonSound.currentTime = 0;
+        buttonSound.play();
+        if (button.classList.contains("number")) {
             display += buttonValue;
             result.value = display;
-        } else {
-            
+
+        } else if (button.classList.contains("all-clear")) {
+            result.value = "";
+            display = "";
+        } else if (button.classList.contains("clear")) {
+            display = display.slice(0, -1);
+            result.value = display;
         }
     });
 });
 
-
+console.log(acButton);
